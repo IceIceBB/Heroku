866df80a93eeb0e51ec37fb7ac9b3ce2caca452e
Leo Montegrande <lmontegrande91@aol.com>
2016-09-23 22:02:17 -0700

    - MondoDB set up

---
+++ models/Icebreaker.js
@@ -0,0 +1,9 @@
+﻿var mongoose = require('mongoose');
+
+var IcebreakerSchema = mongoose.Schema({
+    title: String
+});
+
+var Icebreaker = mongoose.model('Icebreaker', IcebreakerSchema);
+
+module.exports = Icebreaker;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 ﻿var express = require('express');
 var router = express.Router();
 
+var Icebreaker = require('../models/Icebreaker.js');
+
 /* GET users listing. */
 router.get('/', function (req, res) {
     res.send('respond with a resource');
+});
+
+router.post('/', function (req, res) {
+    Icebreaker.create(req.body, function (err, icebreaker) {
+        res.json(icebreaker);
+    });
 });
 
 module.exports = router;
